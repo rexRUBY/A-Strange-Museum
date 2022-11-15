91b8e4f1f1cabaea9c0279073042380dbe7175bc
Yeseo Park <87340601+rexRUBY@users.noreply.github.com>
2022-11-15 20:17:06 +0900

    충돌감지 전

---
+++ main/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+
+import java.io.*;
+import javax.sound.sampled.*;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        MainPanel mainPanel = new MainPanel();
+    }
+}
